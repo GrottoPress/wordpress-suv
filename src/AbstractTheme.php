2f3dea1214907a02f8c87b9dda3166020e8f314b
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2018-01-09 18:54:02 +0000

    Added `AbstractTheme`, `AbstractChildTheme` and `AbstractPlugin` classes.

---
+++ src/AbstractTheme.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Abstract Theme
+ *
+ * @package GrottoPress\WordPress\SUV
+ * @since 0.2.0
+ *
+ * @author GrottoPress <info@grottopress.com>
+ * @author N Atta Kus Adusei
+ */
+
+declare (strict_types = 1);
+
+namespace GrottoPress\WordPress\SUV;
+
+/**
+ * Abstract Theme
+ *
+ * @since 0.2.0
+ */
+abstract class AbstractTheme extends AbstractApp
+{
+}
