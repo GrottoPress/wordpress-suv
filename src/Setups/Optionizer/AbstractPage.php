24a789f3ebf58cbabf1ac223fbdd37dedad7d1d0
N Atta Kusi Adusei <attakusiadusei@gmail.com>
2020-03-11 12:42:18 +0000

    Add options page (pages that go under the 'Settings' menu)

--- src/Setups/Optionizer/AbstractPage.php
+++ src/Setups/Optionizer/AbstractPage.php
@@ -97,9 +97,39 @@ abstract class AbstractPage
         return $this->sections;
     }
 
+    protected function getHookSuffix(): string
+    {
+        return $this->hookSuffix;
+    }
+
     /**
      * Be sure to set `$this->sections` HERE, in the child class.
      * Doing that in the constructor may be too early; it mighty not work.
      */
-    abstract public function add();
+    public function add()
+    {
+        if (!$this->id) {
+            return;
+        }
+
+        if ($this->capability && !\current_user_can($this->capability)) {
+            return;
+        }
+
+        \add_options_page(
+            $this->title,
+            $this->label,
+            $this->capability,
+            $this->id,
+            $this->callback,
+            $this->position
+        );
+
+        \array_walk(
+            $this->sections,
+            function (AbstractSection $section, string $key) {
+                $section->add();
+            }
+        );
+    }
 }
