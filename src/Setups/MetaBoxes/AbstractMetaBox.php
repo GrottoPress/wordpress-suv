8371110af0688d7411bf98f0a3e5a5e3aacfc812
akadusei <attakusiadusei@gmail.com>
2023-06-29 11:44:22 +0000

    Remove unused import

--- src/Setups/MetaBoxes/AbstractMetaBox.php
+++ src/Setups/MetaBoxes/AbstractMetaBox.php
@@ -4,7 +4,6 @@ declare (strict_types = 1);
 namespace GrottoPress\WordPress\SUV\Setups\MetaBoxes;
 
 use GrottoPress\WordPress\SUV\Setups\AbstractSetup;
-use WP_Post;
 
 abstract class AbstractMetaBox extends AbstractSetup
 {
